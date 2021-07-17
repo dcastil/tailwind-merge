de991e9d4c6eee946347eab12265e80f3f057c31
Daniel Castillo <dcastillok@gmail.com>
2021-07-17 20:01:39 +0200

    add tests for standalone classes with dashes in them

--- tests/standalone-classes.test.ts
+++ tests/standalone-classes.test.ts
@@ -7,4 +7,6 @@ test('merges standalone classes from same group correctly', () => {
     expect(twMerge('inline hover:inline focus:inline hover:block hover:focus:block')).toBe(
         'inline focus:inline hover:block hover:focus:block'
     )
+    expect(twMerge('underline line-through')).toBe('line-through')
+    expect(twMerge('line-through no-underline')).toBe('no-underline')
 })
