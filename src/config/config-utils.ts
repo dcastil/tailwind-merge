5a0f3da3c247f0e4f42fc3b6130156fd8770c70b
Daniel Castillo <dcastillok@gmail.com>
2021-07-17 15:58:17 +0200

    make standalone classes work

---
+++ src/config/config-utils.ts
@@ -0,0 +1,147 @@
+import { Config, DynamicClassValidator } from './types'
+
+export type ConfigUtils = ReturnType<typeof createConfigUtils>
+
+export function createConfigUtils(config: Config) {
+    return {
+        prefix: createPrefixUtils(config),
+        class: createClassUtils(config),
+    }
+}
+
+function createPrefixUtils(config: Config) {
+    const prefixToIndexMap = Object.fromEntries(
+        config.prefixes.map((prefix, index) => [prefix, index])
+    )
+
+    function isValid(maybePrefix: string) {
+        return prefixToIndexMap[maybePrefix] !== undefined
+    }
+
+    function compare(firstPrefix: string, secondPrefix: string) {
+        return prefixToIndexMap[firstPrefix]! - prefixToIndexMap[secondPrefix]!
+    }
+
+    return {
+        isValid,
+        compare,
+    }
+}
+
+interface ClassPartObject {
+    nextPart: Record<string, ClassPartObject>
+    validators: ClassValidator[]
+    classGroupId?: ClassGroupId
+}
+
+interface ClassValidator {
+    classGroupId: ClassGroupId
+    validator: DynamicClassValidator
+}
+
+type ClassGroupId = `dynamicClasses.${string}.${number}` | `standaloneClasses.${number}`
+
+const CLASS_PART_SEPARATOR = '-'
+
+function createClassUtils(config: Config) {
+    const classMap: ClassPartObject = {
+        nextPart: {},
+        validators: [],
+    }
+
+    config.standaloneClasses.forEach((classGroup, index) => {
+        const classPartObject = {
+            classGroupId: `standaloneClasses.${index}`,
+        } as const
+
+        classGroup.forEach((className) => {
+            addToClassMap({
+                classPartObject,
+                path: className.split(CLASS_PART_SEPARATOR),
+                classMap,
+            })
+        })
+    })
+
+    function getGroup(className: string) {
+        const classParts = className.split(CLASS_PART_SEPARATOR)
+
+        // Classes like `-inset-1` produce an empty string as first classPart. We assume that classes for negative values are used correctly and remove it from classParts.
+        if (classParts[0] === '') {
+            classParts.unshift()
+        }
+
+        // TODO: Implement processing of dynamic classes
+
+        return getGroupRecursive(classParts, classMap)
+    }
+
+    return {
+        getGroup,
+    }
+}
+
+interface AddToClassMapProps {
+    classPartObject: Partial<ClassPartObject>
+    path: string[]
+    classMap: ClassPartObject
+}
+
+function addToClassMap({ classPartObject, path, classMap }: AddToClassMapProps) {
+    let currentClassPartObject = classMap
+
+    path.forEach((pathPart) => {
+        if (!currentClassPartObject.nextPart[pathPart]) {
+            currentClassPartObject.nextPart[pathPart] = {
+                nextPart: {},
+                validators: [],
+            }
+        }
+
+        currentClassPartObject = currentClassPartObject.nextPart[pathPart]!
+    })
+
+    const { nextPart, validators, classGroupId } = classPartObject
+
+    if (nextPart) {
+        currentClassPartObject.nextPart = {
+            ...currentClassPartObject.nextPart,
+            ...nextPart,
+        }
+    }
+
+    if (validators) {
+        currentClassPartObject.validators = [...currentClassPartObject.validators, ...validators]
+    }
+
+    if (classGroupId) {
+        currentClassPartObject.classGroupId = classGroupId
+    }
+}
+
+function getGroupRecursive(
+    classParts: string[],
+    classPartObject: ClassPartObject
+): ClassGroupId | undefined {
+    if (classParts.length === 0) {
+        return classPartObject.classGroupId
+    }
+
+    const currentClassPart = classParts[0]!
+    const nextClassPartObject = classPartObject.nextPart[currentClassPart]
+    const classGroupFromNextClassPart = nextClassPartObject
+        ? getGroupRecursive(classParts.slice(1), nextClassPartObject)
+        : undefined
+
+    if (classGroupFromNextClassPart) {
+        return classGroupFromNextClassPart
+    }
+
+    if (classPartObject.validators.length === 0) {
+        return undefined
+    }
+
+    const classRest = classParts.join(CLASS_PART_SEPARATOR)
+
+    return classPartObject.validators.find(({ validator }) => validator(classRest))?.classGroupId
+}
--- src/merge-classlist.ts
+++ src/merge-classlist.ts
@@ -1,4 +1,66 @@
-export function mergeClassList(classList: string) {
-    // TODO: Implement
-    return 'merged'
+import { ConfigUtils } from './config/config-utils'
+
+const PREFIX_SEPARATOR = ':'
+
+export function mergeClassList(classList: string, configUtils: ConfigUtils) {
+    /**
+     * Set of classGroupIds in following format:
+     * - No prefix: `:classGroupId`
+     * - With prefix: `prefix:classGroupId`
+     * @example ':standaloneClasses.1'
+     * @example 'hover:focus:dynamicClasses.bg.2'
+     */
+    const classGroupsInConflict = new Set<string>()
+
+    return (
+        classList
+            .trim()
+            .split(/\s+/)
+            .map((originalClassName) => {
+                const prefixes = originalClassName.split(PREFIX_SEPARATOR)
+                const className = prefixes.pop()!
+
+                const arePrefixesValid = prefixes.every(configUtils.prefix.isValid)
+                const classGroupId = arePrefixesValid
+                    ? configUtils.class.getGroup(className)
+                    : undefined
+
+                if (!classGroupId) {
+                    return {
+                        isTailwindClass: false as const,
+                        originalClassName,
+                    }
+                }
+
+                prefixes.sort(configUtils.prefix.compare)
+
+                return {
+                    isTailwindClass: true as const,
+                    prefix: prefixes.join(PREFIX_SEPARATOR),
+                    classGroupId,
+                    originalClassName,
+                }
+            })
+            .reverse()
+            // Last class in conflict wins, so we need to filter conflicting classes in reverse order.
+            .filter((parsed) => {
+                if (!parsed.isTailwindClass) {
+                    return true
+                }
+
+                const classId = `${parsed.prefix}:${parsed.classGroupId}`
+
+                if (classGroupsInConflict.has(classId)) {
+                    return false
+                }
+
+                classGroupsInConflict.add(classId)
+                // TODO: Add conflictingClasses to classGroupsInConflict
+
+                return true
+            })
+            .reverse()
+            .map((parsed) => parsed.originalClassName)
+            .join(' ')
+    )
 }
