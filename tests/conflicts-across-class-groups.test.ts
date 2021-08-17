68637989eb3abda64051d864737e21b55e44b0cf
Daniel Castillo <31006608+dcastil@users.noreply.github.com>
2021-08-17 20:57:22 +0200

    add test case

--- tests/conflicts-across-class-groups.test.ts
+++ tests/conflicts-across-class-groups.test.ts
@@ -11,3 +11,8 @@ test('handles conflicts across class groups correctly', () => {
     expect(twMerge('right-1 inset-x-1 inset-y-1')).toBe('inset-x-1 inset-y-1')
     expect(twMerge('inset-x-1 hover:left-1 inset-1')).toBe('hover:left-1 inset-1')
 })
+
+test('ring and shadow classes do not create conflict', () => {
+    expect(twMerge('ring shadow')).toBe('ring shadow')
+    expect(twMerge('ring-2 shadow-md')).toBe('ring-2 shadow-md')
+})
