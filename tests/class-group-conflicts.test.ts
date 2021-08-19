1eae4db585588f1b60772ee6878e298434930d1a
Daniel Castillo <31006608+dcastil@users.noreply.github.com>
2021-08-20 00:06:04 +0200

    rename dynamic classes test suite

--- tests/class-group-conflicts.test.ts
+++ tests/class-group-conflicts.test.ts
@@ -1,6 +1,6 @@
 import { twMerge } from '../src'
 
-test('merges dynamic classes from same group correctly', () => {
+test('merges classes from same group correctly', () => {
     expect(twMerge('overflow-x-auto overflow-x-hidden')).toBe('overflow-x-hidden')
     expect(twMerge('overflow-x-auto overflow-x-hidden overflow-x-scroll')).toBe('overflow-x-scroll')
     expect(twMerge('overflow-x-auto hover:overflow-x-hidden overflow-x-scroll')).toBe(
