866eb238c6e751c661fc66e351afdd5c51620a95
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-10-21 19:28:56 +0200

    build `extendTailwindMerge`

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { createTailwindMerge } from './create-tailwind-merge'
 import { getDefaultConfig } from './default-config'
 
 export const twMerge = createTailwindMerge(getDefaultConfig)
+
 export { createTailwindMerge, getDefaultConfig }
+export { extendTailwindMerge } from './extend-tailwind-merge'
 export type { Config } from './types'
 export * as validators from './validators'
 export { mergeConfigs } from './merge-configs'
