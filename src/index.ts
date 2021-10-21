06c3115c0e17d2574b3adc01fd3fdefeef6ef1ff
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-10-21 15:37:32 +0200

    add mergeConfigs function

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { getDefaultConfig } from './default-config'
 
 export const twMerge = createTailwindMerge(getDefaultConfig)
 export { createTailwindMerge, getDefaultConfig }
-export * as validators from './validators'
 export type { Config } from './types'
+export * as validators from './validators'
+export { mergeConfigs } from './merge-configs'
