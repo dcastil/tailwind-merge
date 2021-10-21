033ccb757aa2da8afb2eea973df8fe3a96855a26
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-10-21 19:38:47 +0200

    add extendTailwindMerge test

---
+++ tests/extend-tailwind-merge.test.ts
@@ -0,0 +1,30 @@
+import { extendTailwindMerge } from '../src'
+
+test('extendTailwindMerge works corectly', () => {
+    const tailwindMerge = extendTailwindMerge({
+        cacheSize: 20,
+        prefixes: ['my-prefix'],
+        classGroups: {
+            fooKey: [{ fooKey: ['bar', 'baz'] }],
+            fooKey2: [{ fooKey: ['qux', 'quux'] }, 'other-2'],
+            otherKey: ['nother', 'group'],
+        },
+        conflictingClassGroups: {
+            fooKey: ['otherKey'],
+            otherKey: ['fooKey', 'fooKey2'],
+        },
+    })
+
+    expect(tailwindMerge('')).toBe('')
+    expect(tailwindMerge('my-prefix:fooKey-bar my-prefix:fooKey-baz')).toBe('my-prefix:fooKey-baz')
+    expect(tailwindMerge('other-prefix:fooKey-bar other-prefix:fooKey-baz')).toBe(
+        'other-prefix:fooKey-bar other-prefix:fooKey-baz'
+    )
+    expect(tailwindMerge('group fooKey-bar')).toBe('fooKey-bar')
+    expect(tailwindMerge('fooKey-bar group')).toBe('group')
+    expect(tailwindMerge('group other-2')).toBe('group other-2')
+    expect(tailwindMerge('other-2 group')).toBe('group')
+
+    expect(tailwindMerge('p-10 p-20')).toBe('p-20')
+    expect(tailwindMerge('hover:focus:p-10 focus:hover:p-20')).toBe('focus:hover:p-20')
+})
