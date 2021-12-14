4a196614043d3fef0156212d77aff27b4930c0f8
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-12-14 22:23:08 +0100

    add test case for stroke-width and stroke-color being merged incorrectly

--- tests/tw-merge.test.ts
+++ tests/tw-merge.test.ts
@@ -2,4 +2,5 @@ import { twMerge } from '../src'
 
 test('twMerge', () => {
     expect(twMerge('mix-blend-normal mix-blend-multiply')).toBe('mix-blend-multiply')
+    expect(twMerge('stroke-black stroke-1')).toBe('stroke-black stroke-1')
 })
