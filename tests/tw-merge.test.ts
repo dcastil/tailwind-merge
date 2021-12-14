b53c2f4e12b5ff2b6089c93ccdfc406e8960f810
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-12-14 22:37:10 +0100

    add test case for arbitrary value in some classs groups

--- tests/tw-merge.test.ts
+++ tests/tw-merge.test.ts
@@ -3,4 +3,6 @@ import { twMerge } from '../src'
 test('twMerge', () => {
     expect(twMerge('mix-blend-normal mix-blend-multiply')).toBe('mix-blend-multiply')
     expect(twMerge('stroke-black stroke-1')).toBe('stroke-black stroke-1')
+    expect(twMerge('grayscale-0 grayscale-[50%]')).toBe('grayscale-[50%]')
+    expect(twMerge('grow grow-[2]')).toBe('grow-[2]')
 })
