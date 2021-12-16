258fd736b537e49f8eeb536e460858c337bb7a1e
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-12-16 21:29:01 +0100

    use CommonJS in update-readme script

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -1,32 +1,36 @@
 // File can only be executed with zx
-import 'zx/globals'
-import { pipe } from 'fp-ts/lib/function.js'
+require('zx/globals')
+const { pipe } = require('fp-ts/lib/function')
 
-import { applyVersionedLogoImage } from './helpers/apply-versioned-logo-image.js'
+const { applyVersionedLogoImage } = require('./helpers/apply-versioned-logo-image')
 
-const ROOT_PATH = `${__dirname}/..`
-const README_PATH = `${ROOT_PATH}/README.md`
-const PACKAGE_PATH = `${ROOT_PATH}/package.json`
+async function run() {
+    const ROOT_PATH = `${__dirname}/..`
+    const README_PATH = `${ROOT_PATH}/README.md`
+    const PACKAGE_PATH = `${ROOT_PATH}/package.json`
 
-const [readme, packageJson] = await Promise.all([
-    fs.readFile(README_PATH, { encoding: 'utf-8' }),
-    fs.readFile(PACKAGE_PATH, { encoding: 'utf-8' }),
-])
+    const [readme, packageJson] = await Promise.all([
+        fs.readFile(README_PATH, { encoding: 'utf-8' }),
+        fs.readFile(PACKAGE_PATH, { encoding: 'utf-8' }),
+    ])
 
-const nextReadme = pipe(readme, (readme) => {
-    const { hasPartsToUpdate, updatedText } = applyVersionedLogoImage(readme, packageJson)
+    const nextReadme = pipe(readme, (readme) => {
+        const { hasPartsToUpdate, updatedText } = applyVersionedLogoImage(readme, packageJson)
 
-    if (!hasPartsToUpdate) {
-        throw Error(
-            `${chalk.red('[ERROR]')} Could not find versioned logo image to update in README`
-        )
+        if (!hasPartsToUpdate) {
+            throw Error(
+                `${chalk.red('[ERROR]')} Could not find versioned logo image to update in README`
+            )
+        }
+
+        return updatedText
+    })
+
+    if (nextReadme !== readme) {
+        await fs.writeFile(README_PATH, nextReadme)
     }
 
-    return updatedText
-})
-
-if (nextReadme !== readme) {
-    await fs.writeFile(README_PATH, nextReadme)
+    await $`git add README.md`
 }
 
-await $`git add README.md`
+run()
