e42744999176f3ff10d543b8db941dcc62c98c11
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2022-07-11 10:07:33 +0200

    add test case for w-fit

--- tests/class-group-conflicts.test.ts
+++ tests/class-group-conflicts.test.ts
@@ -2,6 +2,7 @@ import { twMerge } from '../src'
 
 test('merges classes from same group correctly', () => {
     expect(twMerge('overflow-x-auto overflow-x-hidden')).toBe('overflow-x-hidden')
+    expect(twMerge('w-full w-fit')).toBe('w-fit')
     expect(twMerge('overflow-x-auto overflow-x-hidden overflow-x-scroll')).toBe('overflow-x-scroll')
     expect(twMerge('overflow-x-auto hover:overflow-x-hidden overflow-x-scroll')).toBe(
         'hover:overflow-x-hidden overflow-x-scroll'
