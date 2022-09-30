c740e7270172ea24a45600bfb046d639cd6abdde
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2022-09-30 12:37:47 +0200

    add test case

--- tests/tw-merge.test.ts
+++ tests/tw-merge.test.ts
@@ -2,6 +2,7 @@ import { twMerge } from '../src'
 
 test('twMerge', () => {
     expect(twMerge('mix-blend-normal mix-blend-multiply')).toBe('mix-blend-multiply')
+    expect(twMerge('h-10 h-min')).toBe('h-min')
     expect(twMerge('stroke-black stroke-1')).toBe('stroke-black stroke-1')
     expect(twMerge('outline-black outline-1')).toBe('outline-black outline-1')
     expect(twMerge('grayscale-0 grayscale-[50%]')).toBe('grayscale-[50%]')
