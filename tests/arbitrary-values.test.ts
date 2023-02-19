549f8b8956560d2aa7edf4cbd02326371cee8675
LesnoyPudge <chuykin.a01@gmail.com>
2023-02-20 04:52:39 +0500

    add test case for container query length units

--- tests/arbitrary-values.test.ts
+++ tests/arbitrary-values.test.ts
@@ -7,6 +7,9 @@ test('handles simple conflicts with arbitrary values correctly', () => {
             'm-[2px] m-[11svmin] m-[12in] m-[13lvi] m-[14vb] m-[15vmax] m-[16mm] m-[17%] m-[18em] m-[19px] m-[10dvh]',
         ),
     ).toBe('m-[10dvh]')
+    expect(
+        twMerge('h-[10px] h-[11cqw] h-[12cqh] h-[13cqi] h-[14cqb] h-[15cqmin] h-[16cqmax]'),
+    ).toBe('h-[16cqmax]')
     expect(twMerge('z-20 z-[99]')).toBe('z-[99]')
     expect(twMerge('my-[2px] m-[10rem]')).toBe('m-[10rem]')
     expect(twMerge('cursor-pointer cursor-[grab]')).toBe('cursor-[grab]')
