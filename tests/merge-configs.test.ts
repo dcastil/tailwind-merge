5857aac6eda23b29e61c579429fd0c1af1eee281
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2023-08-20 11:32:57 +0200

    adjust tests

--- tests/merge-configs.test.ts
+++ tests/merge-configs.test.ts
@@ -5,52 +5,83 @@ test('mergeConfigs has correct behavior', () => {
         mergeConfigs(
             {
                 cacheSize: 50,
+                prefix: 'tw-',
                 separator: ':',
                 theme: {
                     hi: ['ho'],
+                    themeToOverride: ['to-override'],
                 },
                 classGroups: {
                     fooKey: [{ fooKey: ['one', 'two'] }],
                     bla: [{ bli: ['blub', 'blublub'] }],
+                    groupToOverride: ['this', 'will', 'be', 'overridden'],
+                    groupToOverride2: ['this', 'will', 'not', 'be', 'overridden'],
                 },
-                conflictingClassGroups: {},
+                conflictingClassGroups: {
+                    toOverride: ['groupToOverride'],
+                },
                 conflictingClassGroupModifiers: {
                     hello: ['world'],
+                    toOverride: ['groupToOverride-2'],
                 },
             },
             {
+                separator: '-',
+                prefix: undefined,
+                theme: {
+                    themeToOverride: ['overridden'],
+                },
                 classGroups: {
-                    fooKey: [{ fooKey: ['bar', 'baz'] }],
-                    fooKey2: [{ fooKey: ['qux', 'quux'] }],
-                    otherKey: ['nother', 'group'],
+                    groupToOverride: ['I', 'overrode', 'you'],
+                    groupToOverride2: undefined!,
                 },
                 conflictingClassGroups: {
-                    fooKey: ['otherKey'],
-                    otherKey: ['fooKey', 'fooKey2'],
+                    toOverride: ['groupOverridden'],
                 },
                 conflictingClassGroupModifiers: {
-                    hello: ['world2'],
+                    toOverride: ['overridden-2'],
+                },
+                extend: {
+                    classGroups: {
+                        fooKey: [{ fooKey: ['bar', 'baz'] }],
+                        fooKey2: [{ fooKey: ['qux', 'quux'] }],
+                        otherKey: ['nother', 'group'],
+                        groupToOverride: ['extended'],
+                    },
+                    conflictingClassGroups: {
+                        fooKey: ['otherKey'],
+                        otherKey: ['fooKey', 'fooKey2'],
+                    },
+                    conflictingClassGroupModifiers: {
+                        hello: ['world2'],
+                    },
                 },
             },
         ),
     ).toEqual({
         cacheSize: 50,
-        separator: ':',
+        prefix: 'tw-',
+        separator: '-',
         theme: {
             hi: ['ho'],
+            themeToOverride: ['overridden'],
         },
         classGroups: {
             fooKey: [{ fooKey: ['one', 'two'] }, { fooKey: ['bar', 'baz'] }],
             bla: [{ bli: ['blub', 'blublub'] }],
             fooKey2: [{ fooKey: ['qux', 'quux'] }],
             otherKey: ['nother', 'group'],
+            groupToOverride: ['I', 'overrode', 'you', 'extended'],
+            groupToOverride2: ['this', 'will', 'not', 'be', 'overridden'],
         },
         conflictingClassGroups: {
+            toOverride: ['groupOverridden'],
             fooKey: ['otherKey'],
             otherKey: ['fooKey', 'fooKey2'],
         },
         conflictingClassGroupModifiers: {
             hello: ['world', 'world2'],
+            toOverride: ['overridden-2'],
         },
     })
 })
